d0114b04f479080dc0e9cf0d0106f7ac4a9413aa
marco <z405656232x@163.com>
2017-08-06 17:38:06 +0800

    comment added in bat file

--- dist/1.5/dumi-gui.bat
+++ dist/1.5/dumi-gui.bat
@@ -1,1 +1,5 @@
+:: running with gui mode
 java -jar ./dumi-1.5.jar
+
+:: running with console mode
+:: java -jar ./dumi-1.5.jar -console 1 -f <scanFolder> [-d <userDictionaryFile>] [-r <userRegexPatternFile>] [-t <threadThreshold>]
