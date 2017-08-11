23f732fdea6bd5193ad3e3d6a57fe65a30ca900b
marco <z405656232x@163.com>
2017-08-11 10:02:30 +0800

    remove batch file

--- dist/1.5/dumi-gui.bat
+++
@@ -1,5 +0,0 @@
-:: running with gui mode
-java -jar ./dumi-1.5.jar
-
-:: running with console mode
-:: java -jar ./dumi-1.5.jar -console 1 -f <scanFolder> [-d <userDictionaryFile>] [-r <userRegexPatternFile>] [-t <threadThreshold>]
